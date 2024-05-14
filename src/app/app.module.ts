c41a7c86911b6ab2ee71e351509643201cc12503
tabitha silva <tabithaff@gmail.com>
2024-05-13 23:14:36 -0300

    feat: :sparkles: Adicionando a module.ts para configuração

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, AppComponent, CommonModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
